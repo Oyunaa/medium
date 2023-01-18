ab69b8d9ac92aa14bb428062216caad971e23a78
Oyunaa <oyuka579@gmail.com>
2023-01-19 05:06:32 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,5 +2,9 @@ import React from "react";
 
 export default function Footer() {
   const date = new Date();
-  return <div className="container">Copyright @{date.getFullYear()}</div>;
+  return (
+    <div className="container">
+      Copyright @{date.getFullYear() / date.getMonth() + 1 / date.getDate()}
+    </div>
+  );
 }
