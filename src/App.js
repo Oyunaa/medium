a0ac12de15bdfbcfbe861c2a79152ca3bb6e538c
Oyunaa <oyuka579@gmail.com>
2023-01-19 05:03:52 +0800

    update app word

--- src/App.js
+++ src/App.js
@@ -63,6 +63,8 @@ const App = () => {
             openModal={openModal}
             setAdmin={setAdmin}
           />
+
+          <h3>Hello Page</h3>
           <div>
             <Routes>
               <Route
